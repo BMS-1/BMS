04a93ab1d21eaa9adb837aec3911b522a5ad8e05
kashyap5320 <63612972+kashyap5320@users.noreply.github.com>
2020-06-15 17:26:22 +0530

    add

--- Home.py
+++ Home.py
@@ -1,11 +1,26 @@
-from tkinter import Tk
+from tkinter import *
 
+
+def gotomehsana(self):
+    print("kashyap")
 class home:
     def __init__(self):
         root.geometry ( "700x500" )
+        menu=Menu(root)
+        root.config(menu=menu)
+
+        subMenu=Menu(menu)
+        menu.add_cascade(label="location", menu=subMenu)
+        subMenu.add_command(label="mehsana",command=gotomehsana(self))
 
 
-root=Tk()
+
+
+
+
+
+
+root=Tk ()
 start=home()
 root.mainloop()
 
