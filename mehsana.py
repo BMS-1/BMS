ea38daa71e5121a6981bb0b488cd735b7029c05d
kashyap5320 <63612972+kashyap5320@users.noreply.github.com>
2020-06-15 20:02:35 +0530

    add

--- mehsana.py
+++ mehsana.py
@@ -1,1 +1,20 @@
+from tkinter import  *
 
+class mehsana:
+    def __init__(self):
+        root.geometry("700x500")
+        root.title("mehsana")
+
+
+
+
+
+
+
+root=Tk()
+meh=mehsana()
+photo = PhotoImage(file="1.png")
+label = Label(image=photo,bg="black")
+label.pack(side=TOP)
+
+root.mainloop()
