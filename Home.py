eb5010c039668d525096abb4b2d394267f0a3890
ADMIN <utrivedi330@gmail.com>
2020-06-15 16:37:41 +0530

    add

--- Home.py
+++ Home.py
@@ -1,6 +1,12 @@
 from tkinter import Tk
 
 class home:
-    def __init__(self,start):
-     start=home()
+    def __init__(self):
+        root.geometry ( "700x500" )
+
+
+root=Tk()
+start=home()
+root.mainloop()
+
 
